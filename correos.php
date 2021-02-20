e2af7a04ff538981ea08c705e36d0f2579209b9a
david caparachin ricapa <caparachindavid@gmail.com>
2021-02-19 19:50:01 -0500

    Ingresando la parte de contacto

---
+++ correos.php
@@ -0,0 +1,16 @@
+<?php
+  $destinatario = 'caparachindavid@gmail.com';
+  // Esto es al correo al que se enviara el mensaje
+  $nombre = $_POST['nombre'];
+  $telefono = $_POST['telefono'];
+  $correo = $_POST['correo'];
+  $mensaje = $_POST['mensaje'];
+
+  $header = "Enviado desde la pagina AROBED";
+  $mensajeCompleto = $mensaje."\nSAtentamente: ".$nombre
+
+  mail($destinatario, $correo,$mensajeCompleto,$header);
+  echo '<script>alert('correo enviado exitosamente')</script>';
+  echo '<script>setTimeout(\"location.href='index.html'\",1000)</script>';
+
+?>
